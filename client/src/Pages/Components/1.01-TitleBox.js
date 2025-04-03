62e4174892069d7762c75cb1d87711f3f98424a4
Kaan Buke <kabuke13@gmail.com>
2025-04-03 14:55:55 +0200

    install sass dev dependancy

--- client/src/Pages/Components/1.01-TitleBox.js
+++ client/src/Pages/Components/1.01-TitleBox.js
@@ -3,23 +3,23 @@ import logoWhite from "../../resources/imgs/logo-white.png"
 export default function TitleBox(){
     return(
         <div className="header">
-            <div className="logo-box">
-                <img src={logoWhite} alt="Logo" className="logo"/>
+            <div className="header__logo-box">
+                <img src={logoWhite} alt="Logo" className="header__logo"/>
             </div>
 
-            <div className="text-box">
+            <div className="header__text-box">
                 <h1 className="heading-primary">
-                    <span className="heading-primary-main">
+                    <span className="heading-primary--main">
                         Outdoors
                     </span>
 
-                    <span className="heading-primary-sub">
+                    <span className="heading-primary--sub">
                         Is where life happens
                     </span>
                 </h1>
 
                 {/* two names for the className here */}
-                <a href="#" className="btn btn-white">
+                <a href="#" className="btn btn--white btn--animated">
                     Discover Our Tours
                 </a>
             </div>
