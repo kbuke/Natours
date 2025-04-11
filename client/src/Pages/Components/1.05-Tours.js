470bdf4d7a384523c12c8bc38bcb97cd7c0fa516
Kaan Buke <kabuke13@gmail.com>
2025-04-11 13:42:12 +0200

    complete styling of front card

--- client/src/Pages/Components/1.05-Tours.js
+++ client/src/Pages/Components/1.05-Tours.js
@@ -1,21 +1,53 @@
 
 
 export default function Tours(){
+    const seaExplorerList = [
+        "3 Day Tour",
+        "Up to 30 People",
+        "2 Tour Guides",
+        "Sleep in Cozy Hotels",
+        "Difficulty: Easy"
+    ]
 
-    const tourCards = (cardImg, pictureNo, cardNo) => {
+    const forestHikerList = [
+        "7 Day Tour",
+        "Up to 40 people",
+        "6 Tour Guides",
+        "Sleep in Provided Tents",
+        "Difficulty: Medium"
+    ]
+
+    const snowAdventurerList = [
+        "5 Day Tour",
+        "Up to 15 People",
+        "3 Tour Guides",
+        "Sleep in Provided Tents",
+        "Difficulty: Hard"
+    ]
+
+    const tourCards = (cardNo, heading, tourListArray) => {
         return(
             <div className="col-1-of-3">
                 <div className="card">
                     {/*ONE SIDE OF THE CARD*/}
                     <div className="card__side card__side--front">
-                        <div className={`card__picture card__picture--${pictureNo}`}>
+                        <div className={`card__picture card__picture--${cardNo}`}>
                             &nbsp;
                         </div>
-                        <div className="card__heading">
-
-                        </div>
+                        <h4 className="card__heading">
+                            {/* {heading} */}
+                            <span className={`card__heading-span card__heading-span-${cardNo}`}>
+                                {heading}
+                            </span>
+                        </h4>
                         <div className="card__details">
-
+                            <ul>
+                                {tourListArray?.map((item, index) => (
+                                    <li key={index}>
+                                        {item}
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
                     </div>
 
@@ -35,9 +67,9 @@ export default function Tours(){
             </div>
 
             <div className="row">
-                {tourCards("Card 1", "1", "1")}
-                {tourCards("Card 2", "2", "2")}
-                {tourCards("Card 3", "3", "3")}
+                {tourCards("1", "The Sea Explorer", seaExplorerList)}
+                {tourCards("2", "The Forest Hiker", forestHikerList)}
+                {tourCards("3", "The Snow Adventurer", snowAdventurerList)}
             </div>
         </section>
     )
